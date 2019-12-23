fb306d360acc9978344357f50967a66482446526
Greg Padiasek <no.bowfin@seegnify.com>
2019-12-22 22:05:17 -0500

    Add files via upload

--- examples/build.sh
+++ examples/build.sh
@@ -2,8 +2,9 @@
 
 #
 # Copyright (c) 2019 Greg Padiasek
-# Distributed under the terms of the MIT License.
-# See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT
+# Distributed under the terms of the the 3-Clause BSD License.
+# See the accompanying file LICENSE or the copy at 
+# https://opensource.org/licenses/BSD-3-Clause
 #
 
 ROOT_DIR=$(dirname "$0")
