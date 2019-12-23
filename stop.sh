333594b025321891067a2adbdb04bb82e0a3661e
Greg Padiasek <no.bowfin@seegnify.com>
2019-12-22 22:28:12 -0500

    Delete stop.sh

--- stop.sh
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2019 Greg Padiasek
-# Distributed under the terms of the the 3-Clause BSD License.
-# See the accompanying file LICENSE or the copy at 
-# https://opensource.org/licenses/BSD-3-Clause
-#
-
-pkill -9 eagle
